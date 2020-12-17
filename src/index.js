3e910ab332d803afb590160d2955727bbcbcdbe2
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2020-12-17 18:07:01 +0100

    :poop:

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
 import React from "react";
 import { render } from "react-dom";
+import "./index.css";
 
-function Greeting() {
+const BookList = () => {
   return (
-    <div>
-      Hello broskerinos
-    </div>
-  )
-}
+    <section className="booklist">
+      <Book job='asdf' />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+};
 
-export default Greeting
+const Image = () => {
+  return (
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/41SG4SO4T9L._SX351_BO1,204,203,200_.jpg"
+      alt="book pic"
+    />
+  );
+};
 
+const Book = (props) => {
+  console.log(props)
+  return (
+    <article className='book'>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
 
-render(<Greeting />, document.getElementById("root"));
+const Title = () => {
+  return <h1> Ikigai: The Japanese Secret to a Long and Happy Life </h1>;
+};
+
+const Author = () => {
+  return <h4> Hector Garcia </h4>;
+};
+
+render(<BookList />, document.getElementById("root"));
